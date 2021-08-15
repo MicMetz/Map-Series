49277e960c4af944dde5bd27492c66542fc19f6a
MicDoc <47361643+MicMetz@users.noreply.github.com>
2021-08-15 14:06:33 -0600

    Narrowed scope of GIS frame implementation.

--- src/components/mapframe/SelectedSceneView.tsx
+++ src/components/mapframe/SelectedSceneView.tsx
@@ -1,87 +1,132 @@
 import {Box} from '@material-ui/core';
 import * as React from "react";
+
 import {useRef, useState, useEffect} from "react";
 import {loadModules} from "esri-loader";
 import MapsLayer from "./MapsLayer";
+import MapView from "@arcgis/core/views/MapView";
+import WebMap from "@arcgis/core/WebMap";
+import {makeStyles, createStyles} from "@material-ui/styles";
+import AppFrame from "../app/AppFrame";
+
+/*
+
+ type Props = {
+ onSetSceneView: (selectedSceneView: any) => void;
+ }
+
+ export default function SelectedSceneView(props: Props) {
+ const sceneViewRef              = useRef(null);
+ const [sceneView, setSceneView] = useState(null);
+
+ useEffect(() => {
+ loadModules([
+ "esri/config",
+ "esri/Map",
+ "esri/views/SceneView",
+ "esri/layers/Layer",
+ "esri/layers/ElevationLayer",
+ ]).then(([esriConfig, Map, SceneView, Layer]) => {
+ esriConfig.request.httpsDomains = [""];
 
 
-type Props = {
-	onSetSceneView: (selectedSceneView: any) => void;
-}
+ const mapLayer = new Layer({
+ portalItem: {
+ id: "d2632738d98e40818795ee1c8a2efaff",
+ },
+ });
 
-export default function SelectedSceneView(props: Props) {
-	const sceneViewRef              = useRef(null);
-	const [sceneView, setSceneView] = useState(null);
+ const mapsceneView = new SceneView({
+ map: new Map({
+ basemap: "gray-vector",
+ // layers : [mapLayer],
+ }),
+ // container: "viewDiv",
+ container         : sceneViewRef.current,
+ qualityProfile    : "high",
+ spatialReference  : {
+ wkid: 104971,
+ },
+ tilingSchemeLocked: true,
+ camera            : {
+ position: {
+ x               : -112,
+ y               : -25,
+ z               : 1000000,
+ spatialReference: 104971,
+ },
+ heading : 29,
+ tilt    : 49,
+ },
+ zoom              : 7,
+ center            : [-87, 34]
+ });
 
-	useEffect(() => {
-		loadModules([
-			"esri/config",
-			"esri/Map",
-			"esri/views/SceneView",
-			"esri/layers/WMTSLayer",
-			"esri/layers/ElevationLayer",
-		]).then(([esriConfig, Map, SceneView, WMTSLayer]) => {
-			esriConfig.request.httpsDomains = ["geoserver.mars26.com"];
+ mapsceneView.ui.move("zoom", "bottom-left");
+ mapsceneView.ui.move("navigation-toggle", "bottom-left");
+ mapsceneView.ui.move("compass", "bottom-left");
+ mapsceneView.ui.remove("attribution");
 
-			const marsImagery = new WMTSLayer({
-				url        : "https://webdevgroupcu.org/mime9599/DEV/WarDriver/WarDriver2D-5/",
-				activeLayer: {
-					id: "marsmaptiles:marsmaptiles",
-				},
-				serviceMode: "KVP",
-			});
+ setSceneView(mapsceneView);
+ props.onSetSceneView(mapsceneView);
+ }).catch((error) => {
+ console.error("Error while constructing SceneView:", error);
+ });
 
-			const mapsceneView = new SceneView({
-				tilingSchemeLocked: true,
-				map               : new Map({
-					ground: {
-						// layers: [marsElevation],
-					},
-					layers: [marsImagery],
-				}),
-				container         : sceneViewRef.current,
-				qualityProfile    : "high",
-				// setting the spatial reference for Mars_2000 coordinate system
-				spatialReference: {
-					wkid: 104971,
-				},
-				camera          : {
-					position: {
-						x               : -112,
-						y               : -25,
-						z               : 1000000,
-						spatialReference: 104971,
-					},
-					heading : 29,
-					tilt    : 49,
-				},
-			});
+ return () => {
+ if (sceneView) {
+ (sceneView as any).destroy();
+ props.onSetSceneView(null);
+ }
+ console.log(sceneView);
 
-			mapsceneView.ui.move("zoom", "bottom-left");
-			mapsceneView.ui.move("navigation-toggle", "bottom-left");
-			mapsceneView.ui.move("compass", "bottom-left");
-			mapsceneView.ui.remove("attribution");
+ };
+ }, [props, sceneView]);
 
-			setSceneView(mapsceneView);
-			props.onSetSceneView(mapsceneView);
-		}).catch((error) => {
-			console.error("Error while constructing SceneView:", error);
-		});
 
-		return () => {
-			if (sceneView) {
-				(sceneView as any).destroy();
-				props.onSetSceneView(null);
-			}
-		};
-	}, [props, sceneView]);
+ return (
+ <div className = "h-full">
+ <div className={sceneView}/>
+ {/!*<MapsLayer sceneView = {sceneView}/>*!/}
+ </div>
+ );
+ }
+
+ */
+
+
+const useStyles = makeStyles(() =>
+	createStyles({
+		container: {
+			height  : '90vh',
+			width   : '100vw',
+			overflow: "hidden",
+		},
+		mapDiv   : {
+			height: '100%',
+			width : '100%'
+		},
+	}));
+
+const map = new WebMap({
+	basemap: "streets-night-vector",
+});
+
+const view = new MapView({
+	map      : map,
+	container: "viewDiv",
+	center   : [-118.244, 34.052],
+	zoom     : 5
+});
+
+
+export default function SelectedSceneView() {
+	const classes = useStyles();
 
 
 	return (
-		<Box sx = {{display: "block"}}>
-			<div ref = {sceneViewRef} className = "h-full"/>
-			<MapsLayer sceneView = {sceneView}/>
-		</Box>
+		<div className = {classes.container}>
+			<div id = "viewDiv" className = {classes.mapDiv}/>
+		</div>
 	);
-}
-
+};
--- src/pages/FramesPage.tsx
+++ src/pages/FramesPage.tsx
@@ -2,21 +2,17 @@ import React, {useEffect, useState, useRef} from "react";
 import AppFrame from "components/app/AppFrame";
 import {Theme} from '@material-ui/core/styles';
 import {makeStyles, createStyles} from '@material-ui/styles';
-import {loadModules} from "esri-loader";
-import {Typography, Divider, Box, Card, CardMedia, CardContent, CardActions, CardActionArea, Button} from "@material-ui/core";
-import Collapse from '@material-ui/core/Collapse';
+import {Typography, Divider, Box, Card, CardMedia, CardContent, CardActions, CardActionArea, Button, FormControlLabel, Switch, Slide, Collapse} from "@material-ui/core";
 import SelectedSceneView from "components/mapframe/SelectedSceneView"
 import useSetMapsQueryParams from "../store/useMapQueryParams";
 
 
-
-const useStyles = makeStyles((/*theme: Theme*/) =>
+const useStyles = makeStyles(() =>
 	createStyles({
 		root    : {
 			display       : 'flex',
 			flexWrap      : 'wrap',
 			width         : "100%",
-			marginBottom  : "5vh",
 			justifyContent: "center",
 			alignItems    : "center",
 			overflow      : 'hidden',
@@ -24,6 +20,7 @@ const useStyles = makeStyles((/*theme: Theme*/) =>
 		},
 		expand  : {
 			maxWidth: "calc(100% / 2)",
+
 			// transform: 'rotate(180deg)',
 		},
 		avatar  : {
@@ -53,15 +50,20 @@ const useStyles = makeStyles((/*theme: Theme*/) =>
 				opacity: [0.7, 0.8, 0.6],
 			},
 		},
-
+		contain : {
+			width: "100vw"
+		},
+		form : {
+			position: 'fixed'
+		}
 	}),
 );
 
 
 export default function FramesPage() {
-	const classes                   = useStyles();
-	const [showFrame, setShow]      = React.useState(false);
-	const [mapSceneView, setSceneView] = useState(null);
+	const classes                      = useStyles();
+	const [showFrame, setShow]         = React.useState(false);
+	const [mapSceneView, setSceneView] = React.useState<null | any>(null);
 	useSetMapsQueryParams(mapSceneView);
 
 	const handleSelectionClick = () => {
@@ -71,163 +73,163 @@ export default function FramesPage() {
 
 	return (
 		<AppFrame>
-			<Typography variant = "h1" fontSize = {"4rem"} marginLeft = {5} marginBottom = {5}>GIS Map Frames</Typography>
+			 {/* <Typography variant = "h1" fontSize = {"4rem"} marginLeft = {5} marginBottom = {5}>GIS Map Frames</Typography>
+			<FormControlLabel control = {<Switch checked = {showFrame} onChange = {handleSelectionClick}/>} label = "Show Frame?" className={classes.form}/>
+			<Slide direction = "up" in = {showFrame} mountOnEnter unmountOnExit>
+				<Box className = {classes.root}>
+					<SelectedSceneView/>
+				</Box>
+			</Slide>
+			<Collapse in = {!showFrame} className = {classes.contain}>
+				<Box className = {classes.root}>
+					<Card className = {classes.cardItem}>
+						<CardActionArea>
+							<CardMedia component = "img" alt = "" height = "140" image = "https://i.imgur.com/BxBuWTQ.png" title = "test"/>
+							<CardContent className = {classes.content}>
+								<Typography gutterBottom variant = "h5" component = "h2">
+									TEST
+								</Typography>
+								<Typography variant = "body2" color = "textSecondary" component = "p">
+									TEST
+								</Typography>
+							</CardContent>
+						</CardActionArea>
+						<Divider variant = "middle"/>
+						<CardActions disableSpacing className = {classes.actions}>
+							<Button size = "small" color = "primary">Share</Button>
+							<Button size = "small" color = "primary">Learn More</Button>
+						</CardActions>
+					</Card>
+
+					<Card className = {classes.cardItem}>
+						<CardActionArea>
+							<CardMedia
+								component = "img"
+								alt = ""
+								height = "140"
+								image = "https://i.imgur.com/BxBuWTQ.png"
+								title = "test"
+							/>
+							<CardContent className = {classes.content}>
+								<Typography gutterBottom variant = "h5" component = "h2">
+									TEST
+								</Typography>
+								<Typography variant = "body2" color = "textSecondary" component = "p">
+									TEST
+								</Typography>
+							</CardContent>
+						</CardActionArea>
+						<Divider variant = "middle"/>
+						<CardActions disableSpacing className = {classes.actions}>
+							<Button size = "small" color = "primary">Share</Button>
+							<Button size = "small" color = "primary">Learn More</Button>
+						</CardActions>
+					</Card>
+
+					<Card className = {classes.cardItem}>
+						<CardActionArea>
+							<CardMedia
+								component = "img"
+								alt = ""
+								height = "140"
+								image = "https://i.imgur.com/BxBuWTQ.png"
+								title = "test"
+							/>
+							<CardContent className = {classes.content}>
+								<Typography gutterBottom variant = "h5" component = "h2">
+									TEST
+								</Typography>
+								<Typography variant = "body2" color = "textSecondary" component = "p">
+									TEST
+								</Typography>
+							</CardContent>
+						</CardActionArea>
+						<Divider variant = "middle"/>
+						<CardActions disableSpacing className = {classes.actions}>
+							<Button size = "small" color = "primary">Share</Button>
+							<Button size = "small" color = "primary">Learn More</Button>
+						</CardActions>
+					</Card>
+
+					<Card className = {classes.cardItem}>
+						<CardActionArea>
+							<CardMedia
+								component = "img"
+								alt = ""
+								height = "140"
+								image = "https://i.imgur.com/BxBuWTQ.png"
+								title = "test"
+							/>
+							<CardContent className = {classes.content}>
+								<Typography gutterBottom variant = "h5" component = "h2">
+									TEST
+								</Typography>
+								<Typography variant = "body2" color = "textSecondary" component = "p">
+									TEST
+								</Typography>
+							</CardContent>
+						</CardActionArea>
+						<Divider variant = "middle"/>
+						<CardActions disableSpacing className = {classes.actions}>
+							<Button size = "small" color = "primary">Share</Button>
+							<Button size = "small" color = "primary">Learn More</Button>
+						</CardActions>
+					</Card>
+
+					<Card className = {classes.cardItem}>
+						<CardActionArea>
+							<CardMedia
+								component = "img"
+								alt = ""
+								height = "140"
+								image = "https://i.imgur.com/BxBuWTQ.png"
+								title = "test"
+							/>
+							<CardContent className = {classes.content}>
+								<Typography gutterBottom variant = "h5" component = "h2">
+									TEST
+								</Typography>
+								<Typography variant = "body2" color = "textSecondary" component = "p">
+									TEST
+								</Typography>
+							</CardContent>
+						</CardActionArea>
+						<Divider variant = "middle"/>
+						<CardActions disableSpacing className = {classes.actions}>
+							<Button size = "small" color = "primary">Share</Button>
+							<Button size = "small" color = "primary">Learn More</Button>
+						</CardActions>
+					</Card>
+
+					<Card className = {classes.cardItem}>
+						<CardActionArea>
+							<CardMedia
+								component = "img"
+								alt = ""
+								height = "140"
+								image = "https://i.imgur.com/BxBuWTQ.png"
+								title = "test"
+							/>
+							<CardContent className = {classes.content}>
+								<Typography gutterBottom variant = "h5" component = "h2">
+									TEST
+								</Typography>
+								<Typography variant = "body2" color = "textSecondary" component = "p">
+									TEST
+								</Typography>
+							</CardContent>
+						</CardActionArea>
+						<Divider variant = "middle"/>
+						<CardActions disableSpacing className = {classes.actions}>
+							<Button size = "small" color = "primary">Share</Button>
+							<Button size = "small" color = "primary">Learn More</Button>
+						</CardActions>
+					</Card>
+				</Box>
+			</Collapse>*/}
 			<Box className = {classes.root}>
-
-				<Card className = {classes.cardItem}>
-					<CardActionArea>
-						<CardMedia
-							component = "img"
-							alt = ""
-							height = "140"
-							image = "https://i.imgur.com/BxBuWTQ.png"
-							title = "test"
-						/>
-						<CardContent className = {classes.content}>
-							<Typography gutterBottom variant = "h5" component = "h2">
-								TEST
-							</Typography>
-							<Typography variant = "body2" color = "textSecondary" component = "p">
-								TEST
-							</Typography>
-						</CardContent>
-					</CardActionArea>
-					<Divider variant = "middle"/>
-					<CardActions disableSpacing className = {classes.actions}>
-						<Button size = "small" color = "primary">Share</Button>
-						<Button size = "small" color = "primary">Learn More</Button>
-					</CardActions>
-				</Card>
-
-				<Card className = {classes.cardItem}>
-					<CardActionArea>
-						<CardMedia
-							component = "img"
-							alt = ""
-							height = "140"
-							image = "https://i.imgur.com/BxBuWTQ.png"
-							title = "test"
-						/>
-						<CardContent className = {classes.content}>
-							<Typography gutterBottom variant = "h5" component = "h2">
-								TEST
-							</Typography>
-							<Typography variant = "body2" color = "textSecondary" component = "p">
-								TEST
-							</Typography>
-						</CardContent>
-					</CardActionArea>
-					<Divider variant = "middle"/>
-					<CardActions disableSpacing className = {classes.actions}>
-						<Button size = "small" color = "primary">Share</Button>
-						<Button size = "small" color = "primary">Learn More</Button>
-					</CardActions>
-				</Card>
-
-				<Card className = {classes.cardItem}>
-					<CardActionArea>
-						<CardMedia
-							component = "img"
-							alt = ""
-							height = "140"
-							image = "https://i.imgur.com/BxBuWTQ.png"
-							title = "test"
-						/>
-						<CardContent className = {classes.content}>
-							<Typography gutterBottom variant = "h5" component = "h2">
-								TEST
-							</Typography>
-							<Typography variant = "body2" color = "textSecondary" component = "p">
-								TEST
-							</Typography>
-						</CardContent>
-					</CardActionArea>
-					<Divider variant = "middle"/>
-					<CardActions disableSpacing className = {classes.actions}>
-						<Button size = "small" color = "primary">Share</Button>
-						<Button size = "small" color = "primary">Learn More</Button>
-					</CardActions>
-				</Card>
-
-				<Card className = {classes.cardItem}>
-					<CardActionArea>
-						<CardMedia
-							component = "img"
-							alt = ""
-							height = "140"
-							image = "https://i.imgur.com/BxBuWTQ.png"
-							title = "test"
-						/>
-						<CardContent className = {classes.content}>
-							<Typography gutterBottom variant = "h5" component = "h2">
-								TEST
-							</Typography>
-							<Typography variant = "body2" color = "textSecondary" component = "p">
-								TEST
-							</Typography>
-						</CardContent>
-					</CardActionArea>
-					<Divider variant = "middle"/>
-					<CardActions disableSpacing className = {classes.actions}>
-						<Button size = "small" color = "primary">Share</Button>
-						<Button size = "small" color = "primary">Learn More</Button>
-					</CardActions>
-				</Card>
-
-				<Card className = {classes.cardItem}>
-					<CardActionArea>
-						<CardMedia
-							component = "img"
-							alt = ""
-							height = "140"
-							image = "https://i.imgur.com/BxBuWTQ.png"
-							title = "test"
-						/>
-						<CardContent className = {classes.content}>
-							<Typography gutterBottom variant = "h5" component = "h2">
-								TEST
-							</Typography>
-							<Typography variant = "body2" color = "textSecondary" component = "p">
-								TEST
-							</Typography>
-						</CardContent>
-					</CardActionArea>
-					<Divider variant = "middle"/>
-					<CardActions disableSpacing className = {classes.actions}>
-						<Button size = "small" color = "primary">Share</Button>
-						<Button size = "small" color = "primary">Learn More</Button>
-					</CardActions>
-				</Card>
-
-				<Card className = {classes.cardItem}>
-					<CardActionArea>
-						<CardMedia
-							component = "img"
-							alt = ""
-							height = "140"
-							image = "https://i.imgur.com/BxBuWTQ.png"
-							title = "test"
-						/>
-						<CardContent className = {classes.content}>
-							<Typography gutterBottom variant = "h5" component = "h2">
-								TEST
-							</Typography>
-							<Typography variant = "body2" color = "textSecondary" component = "p">
-								TEST
-							</Typography>
-						</CardContent>
-					</CardActionArea>
-					<Divider variant = "middle"/>
-					<CardActions disableSpacing className = {classes.actions}>
-						<Button size = "small" color = "primary">Share</Button>
-						<Button size = "small" color = "primary">Learn More</Button>
-					</CardActions>
-				</Card>
+				<SelectedSceneView/>
 			</Box>
-
-			{/*<Box className = {classes.expand}>
-				<SelectedSceneView onSetSceneView={setSceneView}/>
-			</Box>*/}
 		</AppFrame>
 	);
 };
